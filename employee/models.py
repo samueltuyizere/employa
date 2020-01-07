79eabeb1df9d0c6da750dca06bc31ebe9987795d
TUYIZERE Samuel <sam@ejobu.rw>
2020-01-07 23:07:04 +0200

    added phone number on the employee model

--- employee/models.py
+++ employee/models.py
@@ -16,12 +16,13 @@ POSITION_CHOICES = (
 class Employee(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     first_name = models.CharField(max_length=120)
-    last_name = models.CharField(max_legth=120)
-    national_id = models.IntegerField(unique=True, max_length=16)
+    last_name = models.CharField(max_length=120)
+    phone_number = models.CharField(unique=True, max_length=10)
+    national_id = models.CharField(unique=True, max_length=16)
     email = models.EmailField(unique=True)
     date_of_birth = models.DateField(auto_now=False)
-    status = models.CharField(choices=STATUS_CHOICES, default="Inactive")
-    position = models.CharField(choices=POSITION_CHOICES, default="Developer")
+    status = models.CharField(choices=STATUS_CHOICES, default="Inactive", max_length=120)
+    position = models.CharField(choices=POSITION_CHOICES, default="Developer", max_length=120)
 
 
     def __str__(self):
