3ab73221227556bd0e99c78a4920edefbaeec576
TUYIZERE Samuel <sam@ejobu.rw>
2020-01-07 23:00:01 +0200

    added the employee model

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+STATUS_CHOICES = (
+    ('Inactive', 'Inactive'),
+    ('Active', 'Active')
+)
+POSITION_CHOICES = (
+    ('Manager', 'Manager'),
+    ('Developer', 'Developer'),
+    ('Designer', 'Designer'),
+    ('Intern', 'Intern'),
+)
+
+class Employee(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    first_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_legth=120)
+    national_id = models.IntegerField(unique=True, max_length=16)
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField(auto_now=False)
+    status = models.CharField(choices=STATUS_CHOICES, default="Inactive")
+    position = models.CharField(choices=POSITION_CHOICES, default="Developer")
+
+
+    def __str__(self):
+        return self.id
