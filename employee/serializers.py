2ca8502d25642ebf3f201203846e0228b6a43c6b
TUYIZERE Samuel <sam@ejobu.rw>
2020-01-07 23:25:50 +0200

    defined the fields on employeeserializer

--- employee/serializers.py
+++ employee/serializers.py
@@ -3,5 +3,5 @@ from .models import Employee
 
 
 class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
-    model = EmployeeSerializer
-    fields = all
+    model = Employee
+    fields = ('url', 'first_name', 'last_name', 'phone_number', 'national_id', 'email', 'date_of_birth', 'status', 'postion')
