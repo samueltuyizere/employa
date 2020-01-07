c4c6c987cff9ada84b34a2b6e26ef632e4e301a6
TUYIZERE Samuel <sam@ejobu.rw>
2020-01-08 00:31:33 +0200

    collected a typo in serializers

--- employee/serializers.py
+++ employee/serializers.py
@@ -5,4 +5,4 @@ from .models import Employee
 class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Employee
-        fields = ('url', 'first_name', 'last_name', 'phone_number', 'national_id', 'email', 'date_of_birth', 'status', 'postion')
+        fields = ('url', 'first_name', 'last_name', 'phone_number', 'national_id', 'email', 'date_of_birth', 'status', 'position')
